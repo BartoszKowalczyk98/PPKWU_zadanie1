f58cd792d80aa457e5f188e34428af8b45111bfc
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-10-13 15:25:57 +0200

    Moved reverser class to main application for less clutter in project
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- src/main/java/PPKWU_zadanie1/PpkwuZadanie1Application.java
+++ src/main/java/PPKWU_zadanie1/PpkwuZadanie1Application.java
@@ -2,6 +2,7 @@ package PPKWU_zadanie1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +13,12 @@ public class PpkwuZadanie1Application {
 		SpringApplication.run(PpkwuZadanie1Application.class, args);
 	}
 
+}
+
+@RestController
+class InputReverser{
+	@RequestMapping(path = "/rev/{var}")
+	public String reverseInput( @PathVariable String var){
+		return new String(new StringBuilder(var).reverse());
+	}
 }
