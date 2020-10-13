49770ec68239a9e1cab05c5f7a06e561c5b92e3b
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-10-13 15:00:39 +0200

    Initial commit project base generated using Spring Initializr https://start.spring.io/

---
+++ src/main/java/PPKWU_zadanie1/PpkwuZadanie1Application.java
@@ -0,0 +1,13 @@
+package PPKWU_zadanie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuZadanie1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuZadanie1Application.class, args);
+	}
+
+}
